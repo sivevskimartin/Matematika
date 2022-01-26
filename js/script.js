d2a4a7d370cedeab81e6a3df9e8576b5e9ba32d8
sivevskimartin <63673317+sivevskimartin@users.noreply.github.com>
2022-01-26 18:40:15 +0100

    addSquare

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 function circleArea() {
     var r = document.getElementById('r').value;
     var PI = 3.141;
-    document.getElementById('plostina').value = r*r*PI;
-    document.getElementById('perimetar').value = 2*r*PI;
+    document.getElementById('plostina').value = r*r*PI + " " + "cm";
+    document.getElementById('perimetar').value = 2*r*PI + " " + "cm";
 }
 circleArea();
+
+function squareArea() {
+    var a = document.getElementById('a').value;
+    document.getElementById('plostinaNaKvadrat').value = a*a + " " + "cm";
+    document.getElementById('perimetarNaKvadrat').value = 4*a + " " + "cm";
+}
+squareArea();
