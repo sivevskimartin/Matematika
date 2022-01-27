a51acb076945691840b11c76ba1066dcd4b8337d
sivevskimartin <63673317+sivevskimartin@users.noreply.github.com>
2022-01-27 11:53:57 +0100

    rectangle

--- js/script.js
+++ js/script.js
@@ -12,3 +12,11 @@ function squareArea() {
     document.getElementById('perimetarNaKvadrat').value = 4*a + " " + "cm";
 }
 squareArea();
+
+function rectangleArea() {
+    var a = document.getElementById('aR').value;
+    var b = document.getElementById('bR').value;
+    document.getElementById('plostinaNaPravoagolnik').value = a*b + " " + "cm";
+    document.getElementById('perimetarNaPravoagolnik').value = (2*a)+(2*b) + " " + "cm";
+}
+rectangleArea();
