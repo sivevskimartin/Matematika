c082ead1b470ac9fc087e448d2cb9a0e98d296be
sivevskimartin <63673317+sivevskimartin@users.noreply.github.com>
2022-01-27 17:14:56 +0100

    if

--- js/script.js
+++ js/script.js
@@ -3,20 +3,30 @@ function circleArea() {
     var PI = 3.141;
     document.getElementById('plostina').value = r*r*PI + " " + "cm";
     document.getElementById('perimetar').value = 2*r*PI + " " + "cm";
+    if(r<=0) {
+        alert("БРОЈОТ ШТО ГО ВНЕСОВТЕ НЕМОЖЕ ДА БИДЕ РАДИУС");
+    }
 }
-circleArea();
 
 function squareArea() {
     var a = document.getElementById('a').value;
     document.getElementById('plostinaNaKvadrat').value = a*a + " " + "cm";
     document.getElementById('perimetarNaKvadrat').value = 4*a + " " + "cm";
+    if(a<=0) {
+        alert("БРОЈОТ ШТО ГО ВНЕСОВТЕ НЕМОЖЕ ДА БИДЕ СТРАНА НА КВАДРАТ");
+    }
 }
-squareArea();
+
 
 function rectangleArea() {
     var a = document.getElementById('aR').value;
     var b = document.getElementById('bR').value;
     document.getElementById('plostinaNaPravoagolnik').value = a*b + " " + "cm";
     document.getElementById('perimetarNaPravoagolnik').value = (2*a)+(2*b) + " " + "cm";
+    if(a<=0) {
+        alert("БРОЈОТ ШТО ГО ВНЕСОВТЕ ЗА СТРАНА А НЕМОЖЕ ДА БИДЕ СТРАНА НА ПРАВОАГОЛНИК");
+    }
+    if (b<=0){
+        alert("БРОЈОТ ШТО ГО ВНЕСОВТЕ ЗА СТРАНА Б НЕМОЖЕ ДА БИДЕ СТРАНА НА ПРАВОАГОЛНИК");
+    }
 }
-rectangleArea();
